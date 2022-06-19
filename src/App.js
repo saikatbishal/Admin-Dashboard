1b8687497bcaf5eff4650ee23d8276d762ed3515
subratabishal <subratabishal10@gmail.com>
2022-06-19 19:31:14 +0530

    resolved tailwind errors-somewhat

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
   return (
       <div>
-          <h1>Hello React</h1>
+          <h1 className="underline text-3xl">Hello React</h1>
     </div>
   )
 }
